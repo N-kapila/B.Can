940ddcad49a7e25f0e5023056a20076c7d74315c
N-kapila <nirmalkapilarathne1998@gmail.com>
2025-01-02 14:54:26 +0530

    dropdown services done

--- src/layouts/main/MenuConfig.js
+++ src/layouts/main/MenuConfig.js
@@ -23,15 +23,21 @@ const menuConfig = [
     path: PATH_PAGE.admin_settings,
   },
   {
-    title: "Sevices",
+    title: "Services",
     icon: <Iconify icon={"eva:file-fill"} {...ICON_SIZE} />,
     children: [
       {
-        subheader: "Others",
+        subheader: "Mammography",
         items: [
-          { title: "Mammography 1", path: PATH_PAGE.mammography1 },
-          { title: "Mammography 2", path: PATH_PAGE.mammography2 },
-          { title: "Mammography 3", path: PATH_PAGE.mammography3 },
+          { title: "Mammography One", path: PATH_PAGE.mammography1 },
+          { title: "Mammography Two", path: PATH_PAGE.mammography2 },
+          { title: "Mammography Three", path: PATH_PAGE.mammography3 },
+        ],
+      },
+
+      {
+        subheader: "Prediction",
+        items: [
           {
             title: "Risk Level Prediction",
             path: PATH_PAGE.risk_level_prediction,
@@ -48,35 +54,39 @@ const menuConfig = [
             title: "Predict Survival Form",
             path: PATH_PAGE.predict_survival_form,
           },
-          { title: "Chat Box", path: PATH_PAGE.chat_box },
-
-          { title: "temp", path: PATH_PAGE.temp },
-          { title: "Admin Mammo 1", path: PATH_PAGE.admin_mammography1 },
-          { title: "Admin Mammo 2", path: PATH_PAGE.admin_mammography2 },
-          { title: "Admin Mammo 3", path: PATH_PAGE.admin_mammography3 },
-          {
-            title: "Admin Risk Level",
-            path: PATH_PAGE.admin_risk_level_prediction,
-          },
-          {
-            title: "Admin Survival Prediction",
-            path: PATH_PAGE.admin_survival_prediction,
-          },
-          {
-            title: "For you",
-            path: PATH_PAGE.for_you,
-          },
-          { title: "Admin Settings", path: PATH_PAGE.admin_settings },
-          {
-            title: "Admin User Management",
-            path: PATH_PAGE.admin_user_management,
-          },
-          {
-            title: "Admin Knowledge Management",
-            path: PATH_PAGE.admin_knowledge_management,
-          },
         ],
       },
+
+      {
+        subheader: "Chat Bot",
+        items: [{ title: "Chat Box", path: PATH_PAGE.chat_box }],
+      },
+
+      // { title: "temp", path: PATH_PAGE.temp },
+      // { title: "Admin Mammo 1", path: PATH_PAGE.admin_mammography1 },
+      // { title: "Admin Mammo 2", path: PATH_PAGE.admin_mammography2 },
+      // { title: "Admin Mammo 3", path: PATH_PAGE.admin_mammography3 },
+      // {
+      //   title: "Admin Risk Level",
+      //   path: PATH_PAGE.admin_risk_level_prediction,
+      // },
+      // {
+      //   title: "Admin Survival Prediction",
+      //   path: PATH_PAGE.admin_survival_prediction,
+      // },
+      // {
+      //   title: "For you",
+      //   path: PATH_PAGE.for_you,
+      // },
+      // { title: "Admin Settings", path: PATH_PAGE.admin_settings },
+      // {
+      //   title: "Admin User Management",
+      //   path: PATH_PAGE.admin_user_management,
+      // },
+      // {
+      //   title: "Admin Knowledge Management",
+      //   path: PATH_PAGE.admin_knowledge_management,
+      // },
     ],
   },
 ];
