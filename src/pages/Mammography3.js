ffd658be78d71077c3eeccf4343d5996a55c5e4a
N-kapila <nirmalkapilarathne1998@gmail.com>
2025-01-02 12:16:37 +0530

    mom 3 done

--- src/pages/Mammography3.js
+++ src/pages/Mammography3.js
@@ -26,6 +26,7 @@ const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: "center",
+  justifyContent: "center",
 }));
 
 // ----------------------------------------------------------------------
@@ -46,66 +47,71 @@ export default function Mammography3() {
         direction="row"
         justifyContent="space-around"
         alignItems="center"
+        spacing={3}
       >
-        <Grid item sm={5}>
+        <Grid item xs={10} md={5}>
           <Item>
             <h1 style={{ color: "blue", paddingBottom: "20px" }}>
               Do I need a <br />
               mammogram?
             </h1>
 
-            <Stack direction="row" spacing={2}>
-              <Item>
+            <Stack
+              direction={{ xs: "column", sm: "column", lg: "row" }}
+              spacing={2}
+            >
+              <Item sx={{ display: "flex" }}>
                 <Image
                   style={{ width: 200, height: 200, borderRadius: "50%" }}
-                  src="/assets/illustrations/illustration_login.png"
+                  src="/assets/img11.jpg"
                 />
               </Item>
               <Item>
-                <p>
+                <p style={{ fontSize: "20px" }}>
                   Clarifying the <br />
                   confusion about who <br />
                   should be screened and how often.
                 </p>
+                <br />
+                <Button variant="contained" href="#">
+                  {" "}
+                  Read More
+                </Button>
               </Item>
             </Stack>
           </Item>
-          <Box style={{ textAlign: "center", padding: "20px" }}>
-            <Button variant="contained" href="#">
-              {" "}
-              Read More
-            </Button>
-          </Box>
         </Grid>
 
-        <Grid item sm={5}>
+        <Grid item xs={10} md={5}>
           <Item>
             <h1 style={{ color: "blue", paddingBottom: "20px" }}>
               Your first <br />
               mammogram
             </h1>
 
-            <Stack direction="row" spacing={2}>
-              <Item>
+            <Stack
+              direction={{ xs: "column", sm: "column", lg: "row" }}
+              spacing={2}
+            >
+              <Item sx={{ display: "flex" }}>
                 <Image
                   style={{ width: 200, height: 200, borderRadius: "50%" }}
-                  src="/assets/illustrations/illustration_login.png"
+                  src="/assets/img12.png"
                 />
               </Item>
               <Item>
-                <p>
+                <p style={{ fontSize: "20px" }}>
                   What to expect and how to prepare
                   <br /> for your first screening mammogram.
                 </p>
+                <br />
+                <Button variant="contained" href="#">
+                  {" "}
+                  Test Your Mammography{" "}
+                </Button>
               </Item>
             </Stack>
           </Item>
-          <Box style={{ textAlign: "center", padding: "20px" }}>
-            <Button variant="contained" href="#">
-              {" "}
-              Get Started to Test Your Mammography{" "}
-            </Button>
-          </Box>
         </Grid>
       </Grid>
     </Page>
